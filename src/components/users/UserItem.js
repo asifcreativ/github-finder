ea6e313937f0b2a3a065bd119cfe91e3c947e0f2
Muhammad Asif <asifcreativ@gmail.com>
2021-10-13 16:05:09 -0700

    convert class component to function component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export default class UserItem extends Component {
-  //   state = {
-  //     login: "mojombo",
-  //     id: 1,
-  //     avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-  //     html_url: "https://github.com/mojombo",
-  //   };
-
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt={login}
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a className='btn btn-sm btn-dart my-1' href={html_url}>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt={login}
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a className='btn btn-sm btn-dart my-1' href={html_url}>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.prototype = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
