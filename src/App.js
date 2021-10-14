ed38fdca604dd0dfc6527973c6944b72dbf61526
Muhammad Asif <asifcreativ@gmail.com>
2021-10-13 19:17:53 -0700

    implement API call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
+import axios from "axios";
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 
 class App extends Component {
-  name = () => "hey hey";
-  loading = false;
+  state = {
+    users: [],
+    loading: false,
+  };
 
   render() {
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    setTimeout(() => {
+      // TODO: TEST ONLY
+      this.setState({ users: res.data, loading: false });
+    }, 1000);
   }
 }
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,51 +1,29 @@
-import React, { Component } from "react";
+import PropTypes from "prop-types";
+import React from "react";
+import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
-export default class Users extends Component {
-  state = {
-    users: [
-      {
-        login: "mojombo",
-        id: 1,
-        node_id: "MDQ6VXNlcjE=",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/mojombo",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        node_id: "MDQ6VXNlcjI=",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/defunkt",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        node_id: "MDQ6VXNlcjM=",
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/pjhyett",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
   }
-}
+  return (
+    <div style={userStyle}>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr )",
   gridGap: "1rem",
 };
+
+Users.prototype = {
+  users: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+export default Users;
